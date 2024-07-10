e53509ffc7750eafa40e7e7a553bb4d7a5434581
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-07-10 08:24:40 -0700

    feat: create relation from recipe entity to other table

--- src/recipes/entities/recipe.entity.ts
+++ src/recipes/entities/recipe.entity.ts
@@ -4,7 +4,14 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
+import { RecipeIngredient } from './recipeIngredient.entity';
+import { Equipment } from './equipment.entity';
+import { Category } from './category.entity';
+import { Instruction } from './instruction.entity';
 
 @Entity()
 export class Recipe {
@@ -38,4 +45,35 @@ export class Recipe {
     onUpdate: 'CURRENT_TIMESTAMP',
   })
   updated_at: Date;
+
+  @OneToMany(
+    () => RecipeIngredient,
+    (recipeIngredient) => recipeIngredient.recipe,
+  )
+  recipeIngredients: RecipeIngredient[];
+
+  @ManyToMany(() => Equipment, (equipment) => equipment.recipes)
+  @JoinTable({
+    name: 'recipe_equipment',
+    joinColumn: { name: 'id', referencedColumnName: 'id' },
+    inverseJoinColumn: {
+      name: 'equipment_id',
+      referencedColumnName: 'equipment_id',
+    },
+  })
+  equipment: Equipment[];
+
+  @ManyToMany(() => Category, (category) => category.recipes)
+  @JoinTable({
+    name: 'recipe_categories',
+    joinColumn: { name: 'id', referencedColumnName: 'id' },
+    inverseJoinColumn: {
+      name: 'category_id',
+      referencedColumnName: 'category_id',
+    },
+  })
+  categories: Category[];
+
+  @OneToMany(() => Instruction, (instruction) => instruction.recipe)
+  instructions: Instruction[];
 }
