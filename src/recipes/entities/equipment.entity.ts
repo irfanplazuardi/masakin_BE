7e4c05ee4af1a2c911c8a73a2d05976bc9591710
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-07-10 08:23:26 -0700

    feat: create equipment entity

---
+++ src/recipes/entities/equipment.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Recipe } from './recipe.entity';
+
+@Entity()
+export class Equipment {
+  @PrimaryGeneratedColumn()
+  equipment_id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  image_url: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @ManyToMany(() => Recipe, (recipe) => recipe.equipment)
+  recipes: Recipe[];
+}
