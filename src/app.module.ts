d16fee661d9f7abaebc3e795554247548595e8f2
DewaSRY <sdewa6645@gmail.com>
2024-07-10 15:35:07 +0800

    update:write docker-compose, and ready to production

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from './user/user.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { dataSourceOption } from './data-source';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -14,22 +16,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '7d' },
     }),
-    // JwtModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (configService: ConfigService) => ({
-    //     global: true,
-    //     secret: configService.get<string>('JWT_SECRET_KEY'),
-    //     signOptions: { expiresIn: '7d' },
-    //   }),
-    // }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: 5433,
+        port: parseInt(configService.get<string>('DB_PORT')),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
