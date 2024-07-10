1b90c2dbf75265ae795796a6cfa5217be0290f2c
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-07-10 08:23:43 -0700

    feat: create instruction entity

---
+++ src/recipes/entities/instruction.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
+import { Recipe } from './recipe.entity';
+
+@Entity()
+export class Instruction {
+  @PrimaryGeneratedColumn()
+  instruction_id: number;
+
+  @ManyToOne(() => Recipe, (recipe) => recipe.instructions)
+  recipe: Recipe;
+
+  @Column()
+  step_number: number;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ nullable: true })
+  image_url_1: string;
+
+  @Column({ nullable: true })
+  image_url_2: string;
+}
